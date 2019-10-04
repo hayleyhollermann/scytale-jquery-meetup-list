67eef8bd926da9f4b1cc2e085761ab2808af8031
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-04 11:49:54 -0500

    fix table in HTML, function for clicking button increments the total meetups

--- client.js
+++ client.js
@@ -5,18 +5,24 @@ $(document).ready(readyNow);
 
 function addMeetupClick(){
     numberOfMeetups ++;
-    console.log('I was clicked!');
-    $('#meetupCounter').text(numberOfMeetups);
+    //console.log('I was clicked!');
+    $('#meetupCount').text(numberOfMeetups);
+    addRow();
+    //$('table').append('<tr></tr>');
+
+}
+
+function addRow(){
+    $('tbody').append('<tr></tr>');
+
 }
 
 
 
 
 
-
-
 function readyNow() {
-    console.log('we in here!');
-    $('#meetupCounter').text(numberOfMeetups);
+    //console.log('we in here!');
+    $('#meetupCount').text(numberOfMeetups);
     $('#addMeetup').on('click', addMeetupClick)
 }
