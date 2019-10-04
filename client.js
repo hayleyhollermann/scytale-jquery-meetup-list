da63b428af2de30a99b762317c42151eb10e3ac6
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-04 13:00:20 -0500

    append new row with input from the group and date feilds

--- client.js
+++ client.js
@@ -3,19 +3,30 @@ let numberOfMeetups = 0;
 
 $(document).ready(readyNow);
 
+function readyNow() {
+    //console.log('we in here!');
+    $('#meetupCount').text(numberOfMeetups);
+    $('#addMeetup').on('click', addMeetupClick)
+}
+
 function addMeetupClick(){
     numberOfMeetups ++;
     //console.log('I was clicked!');
     $('#meetupCount').text(numberOfMeetups);
-    addRow();
+    //addRow();
     //$('table').append('<tr></tr>');
-
+    let group = $('#groupInput').val();
+    let date = $('#dateInput').val();
+    $('tbody').append('<tr> <th> ' + group + '</th> <th>' + date + '</th> <th>' + '<button class="delete">DELETE!</button>' + '</th> </tr>');
+    //let deleteButton = $('#deleteColumn').attr()
+    console.log(group, date);
+    $('#groupInput').val('')
+    $('#dateInput').val('')
 }
 
-function addRow(){
+/*function addRow(){
     $('tbody').append('<tr></tr>');
-
-}
+}*/
 
 
 
